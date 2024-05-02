a59b305dca8cfd9f0781ae25f71d522eeab0f29a
Daniya <kenebaydaniya@gmail.com>
2024-05-02 23:15:09 +0500

    6th

--- website/__init__.py
+++ website/__init__.py
@@ -20,7 +20,7 @@ def create_app():
     app.register_blueprint(auth, url_prefix = '/')
 
 
-    from .models import Events, User
+    from .models import User
 
     with app.app_context():
         db.create_all()
